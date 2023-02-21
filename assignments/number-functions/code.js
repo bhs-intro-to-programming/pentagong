38461a0830ba0ed8910bf6fc0cda2720e9b5c8a1
pentagong <116295624+pentagong@users.noreply.github.com>
2023-02-21 15:16:28 -0800

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,3 +1,29 @@
+const add = (a, b) => {
+  return a+b
+};
+
 const subtract = (a, b) => {
   return a-b
+};
+
+const multiply = (a, b) => {
+  return a*b
+};
+
+const divide = (a, b) => {
+  return a/b
+};
+
+const mod = (a, b) => {
+  return a%b
+};
+
+const averageOfTwo = (a, b) => {
+  return a*b
+};
+
+const manhattanDistance = (x1, y1, x2, y2) => {
+  const xDistance = Math.abs(x1 - x2);
+  const yDistance = Math.abs(y1 - y2);
+  return xDistance + yDistance;
 };
