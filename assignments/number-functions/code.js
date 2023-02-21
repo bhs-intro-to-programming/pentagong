d99f84149a714aacea6b9cdd01e8284615e80318
pentagong <116295624+pentagong@users.noreply.github.com>
2023-02-21 15:20:04 -0800

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -28,7 +28,7 @@ const manhattanDistance = (x1, y1, x2, y2) => {
   return xDistance + yDistance;
 };
 
-const eucideanDistance = (x1, y1, x2, y2) => {
+const euclideanDistance = (x1, y1, x2, y2) => {
   const xDistance = Math.abs(x1 - x2);
   const yDistance = Math.abs(y1 - y2);
   return Math.sqrt(xDistance*xDistance+yDistance*yDistance);
