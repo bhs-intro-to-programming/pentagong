b575ee8c13fa7d767e339bffe7b0edd0fa3de38e
pentagong <116295624+pentagong@users.noreply.github.com>
2023-02-23 23:08:41 -0800

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -49,10 +49,10 @@ const eatsMice = (red, spotted) => {
 }
 
 const isRed = (red) => {
-  let isFlobby = true= red
-  let isBloggy = true=red
-  let isFlibble = false= !red
-  let isGlobby = false= !red
+  let isFlobby = true = red
+  let isBloggy = true =red
+  let isFlibble = false = !red
+  let isGlobby = false = !red
 
   return red
 }
