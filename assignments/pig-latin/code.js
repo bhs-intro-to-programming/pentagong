08a26566f7678ec07cb96530939a5e71a3a17455
pentagong <116295624+pentagong@users.noreply.github.com>
2023-02-28 14:39:40 -0800

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -36,5 +36,5 @@
 
 const pigLatin = (word) => {
   const v = word.search(/[aeiou]/);
-  return word.substring(v) + word.match(/[aeiou]/) + 'ay'
+  return word.substring(v) + word.match(/[qwrtypsdfghjklzxcvbnm]/) + 'ay'
 };
