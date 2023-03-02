3c9119c694eb2afac2fe33d5ff0bab7a46fad0b1
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-02 15:18:19 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -28,6 +28,7 @@ const oneCircle = (size) => {
   drawFilledCircle(width/2, height/2, size, 'black');
   drawFilledCircle(width/3, height/2, size, 'black');
   drawFilledCircle(width/6, height/2, size, 'black');
+  drawFilledCircle(width/1.5, height/2, size, 'black');
 }
 
 oneCircle(54);
