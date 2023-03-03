404e0afd9496704811530841de46eb9d65a6cdc7
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-03 14:45:04 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -45,7 +45,7 @@ const colorCircle = (size) => {
 
 
 const manyCircles = (size) => {
-  let i = 0;
+  let i = 3;
   while (i < width / size){
     drawFilledCircle(i * size, height/2, size, 'black')
     i++
