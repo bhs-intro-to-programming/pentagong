48b2665f161192b703f199cf5c4ed91b4af8007a
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-10 15:14:43 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -95,7 +95,7 @@ const finalBoard2 = (x,y) => {
     const maxHeight = Math.floor(height);
     for (let i =0; i < maxHeight; i++){
       for (let d = 0; d < maxHeight; d++)
-      drawFilledRect (width/4, height/4, (width/x), (height/x))
+      drawFilledRect (width/4, height/4, (width/x), (height/x), 'black')
       drawFilledCircle(width/4, height/4, y, 'white')
     }
 }
