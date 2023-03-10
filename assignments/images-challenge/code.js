a7d4ed363bacbb26e3a099d153b8695cf22cc5fa
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-10 15:22:05 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -83,7 +83,7 @@ const checkerBoard = (x) => {
   for (let i = 0; i < x; i++){
     for (let d = 0; d < x; d++){
       drawFilledRect(((width - (x*y))/2)+y*d, 
-      ((height+(x*y))/2)+3*y,y,y, 
+      ((height+(x*y))/2)+i*y,y,y, 
       (i+d) %2 === 0 ? 'black':'red')
     }  
   }    
