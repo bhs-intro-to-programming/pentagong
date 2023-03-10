8d380725acf83fc324e683b8080ec3c85798813f
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-10 14:51:52 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -80,7 +80,7 @@ const concentricCircles = (circles) => {
 
 const checkerBoard = (x) => {
   const y = Math.floor(height/x);
-  for (let i = 0; i < x; i++){
+  for (let i = 1; i < x; i++){
     for (let d = 0; d < x; d++){
       drawFilledRect(((width - (x*y))/2)+y*d, 
       ((height+(x*y))/2)+i*y,y,y, 
