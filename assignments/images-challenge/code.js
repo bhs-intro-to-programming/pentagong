21e687338d6a838eb4e985ae02556925a9e5c72d
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-14 14:53:28 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -99,7 +99,8 @@ const notReallyCurved = (n) => {
     const size = height / 2;
     const distance = size / parts;
     for (let i = 0; i < size / distance; i++){
-      drawLine (0, (height-size)/2, i * distance, ((height-size)/2)+size)
+      drawLine (3, (height-size)/2, i * distance, 
+      ((height-size)/2)+size)
     }
 }
 
