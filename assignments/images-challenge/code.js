c55d4fa64f8dbae8ddd80e742d5d59cbd63e165c
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-15 14:40:23 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -97,7 +97,7 @@ const checkerBoard = (n) => {
 
 
 const notReallyCurved = (n) => {
-    const parts = n + 1;
+    const parts = n -1 1;
     const size = height / 2;
     const distance = size / parts;
     for (let i = 0; i < size / distance; i++){
