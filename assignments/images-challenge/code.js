19beda49bda81601d740ce79fba88fc3d3759887
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-17 15:19:59 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -117,7 +117,7 @@ const notReallyCurved = (n) =>{
 
 const fill = (size) => {
     for (let i = 0; i<height; i++){
-      drawCircle(0+size, 0+size, size/2, 'red')
+      drawCircle(i+size, i+size, size/2, 'red')
     }
 
 }
