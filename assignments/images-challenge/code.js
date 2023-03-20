4c35d9621ea8e6ad32969dccdf4b1c1d9937671f
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-20 14:53:34 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -131,7 +131,7 @@ const someArtThatWasCreatedByMistake = (size) => {
    const distance = 2*size;
     for (let i = 0; i<width; i++){
       for (let d=0; d<height; d++){
-      drawCircle(width-(i * 2 * size), height - (distance + 2*d*size)+10, size, 'red')
+      drawCircle(width-(i * 2 * size), height - (distance + 2*d*size)+100, size, 'red')
       
     }
     }
