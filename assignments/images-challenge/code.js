77f08660ef1a45dbfb5f463da15404d994ac3fa3
pentagong <116295624+pentagong@users.noreply.github.com>
2023-03-21 15:00:02 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -133,7 +133,7 @@ const fill = (size) => {
   const distance = 2 * size;
   for (let i = 0; i < w; i++) {
     for (let d = 0; d < h; d++) {
-      drawCircle(width - (i * 2 * size), 
+      drawCircle(w - (i * 2 * size), 
       height - (distance + 2 * d * size), size, 'red')
 
     }
