49445d7bb0bcdd2ed241ba89490eefc2077ee6e2
pentagong <116295624+pentagong@users.noreply.github.com>
2023-04-12 14:47:48 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,9 +1,9 @@
 const countTens = (numbers) => {
   let tens = 0;
-  for(let i = 0; i < numbers; i++){
+  for(let i = 0; i < numbers.length; i++){
     if (numbers === 10)
     {
-      tens++
+      tens++;
     }
   }
 }
