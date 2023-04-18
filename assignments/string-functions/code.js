d305543508207a6ae08341b4b9b08da0a76289d8
pentagong <116295624+pentagong@users.noreply.github.com>
2023-04-18 14:48:40 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -2,7 +2,7 @@ const findFnord = (string) => {
   return (string.indexOf('fnord'))
 }
 
-const stringContains = (string1, string2) => {
+const stringContains = (string1, string2) => {{
   (string1.indexOf(string2))}
   return true
 }
