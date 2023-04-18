26900d0b1c8f7b8c9f92bcb799390ca628b72905
pentagong <116295624+pentagong@users.noreply.github.com>
2023-04-18 14:58:27 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -11,5 +11,5 @@ const firstAndLast = (string) => {
 }
 
 const swapFrontAndBack = (string) => {
-  return string.substring(string.length/2)
+  return string.substring(string.length/2) + string.substring(0,string.length/2)
 }
