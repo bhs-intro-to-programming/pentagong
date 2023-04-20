f911c6f1ca4e5cb07deff575602910082fde3dbc
pentagong <116295624+pentagong@users.noreply.github.com>
2023-04-20 15:29:59 -0700

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -10,6 +10,10 @@ const emptyObject = (object) => {
   return {object}
 }
 
-const distance = (x1, x2) => {
-  return Math.sqrt(((x1.x-x2.x)**2) + ((x1.y-x2.y)**2))
+const distance = (p1, p2) => {
+  return Math.sqrt(((p1.x-p2.x)**2) + ((p1.y-p2.y)**2))
+}
+
+const midpoint = (p1, p2) => {
+  
 }
