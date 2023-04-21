f1944dd4c3cf613eeedee0d3d499287b1c1cc968
pentagong <116295624+pentagong@users.noreply.github.com>
2023-04-21 14:48:08 -0700

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -22,7 +22,7 @@ const midpoint = (p1, p2) => {
 const sumSalaries = (array) => {
   let sum = 0;
   for(let i = 0; i < array.length; i++){
-    return sum = array[i].salary
+    return sum = array[i]
   }
   return sum
 }
@@ -32,5 +32,5 @@ const newHighScore = (hs, po) => {
 }
 
 const summarizeBooks = (array) => {
-  return array['title']
+
 }
